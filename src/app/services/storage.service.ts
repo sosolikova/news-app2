5e4283b8945107a449a71bb1d7f9ef50cf13cdbc
Veronika Sosolikova <v_sosolikova@utb.cz>
2023-02-06 21:51:51 +0100

    Service part

--- src/app/services/storage.service.ts
+++ src/app/services/storage.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Preferences } from '@capacitor/preferences';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,25 @@ import { Injectable } from '@angular/core';
 export class StorageService {
 
   constructor() { }
+
+  async create(key:string, value:any)
+  {
+    await Preferences.set({key,value})
+  }
+  async read(key:string)
+  {
+    return (await Preferences.get({key}))
+  }
+  async update(key:string, value:any)
+  {
+    await Preferences.set({key,value});
+  }
+  async delete (key:string) 
+  {
+    await Preferences.remove({key})
+  }
+  async clear()
+  {
+    await Preferences.clear();
+  }
 }
