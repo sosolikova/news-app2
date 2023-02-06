e5117f429b09c83a15e962dbbf0a819172b7cffa
Veronika Sosolikova <v_sosolikova@utb.cz>
2023-02-06 22:06:40 +0100

    Create function for data maintanance

--- src/app/pages/notebook/notebook.page.ts
+++ src/app/pages/notebook/notebook.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Person } from 'src/app/interfaces/person';
+import { StorageService } from 'src/app/services/storage.service';
 
 @Component({
   selector: 'app-notebook',
@@ -7,7 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NotebookPage implements OnInit {
 
-  constructor() { }
+  person:Person ={name:'', email:''}
+  themeName:string='';
+  public personName: string="";
+  public personEmail: string="";
+
+  constructor(private storage:StorageService) { }
+
+  setStorage()
+  {
+
+  }
+  async getStorage()
+  {
+
+  }
+  async updateStorage()
+  {
+
+  }
+  async deleteFromStorage()
+  {
+
+  }
+  async clearStorage()
+  {
+
+  }
 
   ngOnInit() {
   }
