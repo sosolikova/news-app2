1eb06bbf06b9bb005c67118232c89c97bf281f17
Veronika Sosolikova <v_sosolikova@utb.cz>
2023-02-07 22:22:29 +0100

    Change icons and title AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Inbox', url: '/folder/Inbox', icon: 'mail' },
-    { title: 'Outbox', url: '/folder/Outbox', icon: 'paper-plane' },
-    { title: 'Favorites', url: '/folder/Favorites', icon: 'heart' },
-    { title: 'Archived', url: '/folder/Archived', icon: 'archive' },
-    { title: 'Trash', url: '/folder/Trash', icon: 'trash' },
-    { title: 'Spam', url: '/folder/Spam', icon: 'warning' },
+    { title: 'Obecné', url: '/folder/Inbox', icon: 'earth' },
+    { title: 'Business', url: '/folder/Outbox', icon: 'cash' },
+    { title: 'Sport', url: '/folder/Favorites', icon: 'bicycle' },
+    { title: 'Umění', url: '/folder/Archived', icon: 'videocam' },
+    { title: 'Technologie', url: '/folder/Trash', icon: 'laptop' },
+    { title: 'Zdraví', url: '/folder/Spam', icon: 'medkit' },
+    { title: 'Věda', url: '/folder/Spam', icon: 'rocket' },
   ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
   constructor() {}
 }
